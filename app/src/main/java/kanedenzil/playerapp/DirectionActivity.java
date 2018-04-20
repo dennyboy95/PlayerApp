e165ef66d1174bc7e9b0b8b9f96f7170db3dba67
shubhamChauhan <shubhamchauhan4488@gmail.com>
2018-04-20 19:23:50 -0400

    Distance from flag achieved

--- app/src/main/java/kanedenzil/playerapp/DirectionActivity.java
+++ app/src/main/java/kanedenzil/playerapp/DirectionActivity.java
@@ -1,12 +1,24 @@
 package kanedenzil.playerapp;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,21 +28,19 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DirectionActivity extends AppCompatActivity {
+public class DirectionActivity extends AppCompatActivity implements LocationListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
-    FirebaseDatabase database;
     TextView textView;
-    DatabaseReference root;
-<<<<<<< HEAD
-=======
-    Double latt;
-    Double longg;
-    LocationManager locationManager;
-    private DatabaseReference databaseReference;
-    private ValueEventListener valueEventListener;
+
+    //    private GoogleMap map;
+    private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+//    private static final String NOTIFICATION_MSG = "NOTIFICATION MSG";
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("players");
     private static final String TAG = DirectionActivity.class.getSimpleName();
     Location flagLocation = new Location("");
->>>>>>> 53980a0a64b555c8bd4e7396d2ea1cedcace80d8
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +48,13 @@ public class DirectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_direction);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        database = FirebaseDatabase.getInstance();
-        root = database.getReference();
+
         textView = findViewById(R.id.textView3);
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("players");
+        createGoogleApi();
 
-//       if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-//                != PackageManager.PERMISSION_GRANTED) {
-//
-//            // user did NOT give permission
-//            Log.d("surinderbhago", "no permission granted, requesting p now...");
-//            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 5);
-//
-//
-//        } else {
-
-            // mMap.setMyLocationEnabled(true)
+        googleApiClient.connect();
+        Log.d(TAG, " googleApiClient HAS BEEN CONNECTED");
+        // mMap.setMyLocationEnabled(true)
         String teamName = getIntent().getExtras().getString("team");
         if(teamName == "Team-A") {
 
@@ -62,67 +63,182 @@ public class DirectionActivity extends AppCompatActivity {
         }
         else{
 
-                flagLocation.setLatitude(43.772738);
-                flagLocation.setLongitude(-79.333472);
-
-
+            flagLocation.setLatitude(43.772738);
+            flagLocation.setLongitude(-79.333472);
         }
 
-        getUpdateOnMap();
+//        getUpdateOnMap();
+
     }
 
-    public void getUpdateOnMap(){
-//        = null;
-        if(valueEventListener  == null){
-            valueEventListener = new ValueEventListener() {
-
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    Log.d(TAG, "onDataChange: ==============Sanapshot==========="+dataSnapshot.toString());
-                    List<Player> players =  new ArrayList<>();
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        Player player = snapshot.getValue(Player.class);
-                        players.add(player);
-                        Log.d(TAG, "Name: "+ player.playerName);
-                    }
-                    setPlayerMaker(players);
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            };
-            databaseReference.addValueEventListener(valueEventListener);
-
+    // Create GoogleApiClient instance
+    private void createGoogleApi() {
+        Log.d(TAG, "createGoogleApi()");
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
         }
     }
 
-    public void setPlayerMaker(List<Player> players){
-        if(players.size() != 0) {
-//            map.clear();
-            String value = getIntent().getExtras().getString("name");
-            for (Player player : players) {
-                if(player.playerName == value) {
-                    Location loc = new Location("");
-                    loc.setLatitude(player.latitude);
-                    loc.setLongitude(player.longitude);
+    //Connecting GooGle api client on start()
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+        // Call GoogleApiClient connection when starting the Activity
+        googleApiClient.connect();
+    }
 
+    //Disconnecting google api client on start()
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Disconnect GoogleApiClient when stopping Activity
+        googleApiClient.disconnect();
+    }
 
-                    float distanceInmeters = flagLocation.distanceTo(loc);
+    // MARK : PERMISSIONS HANDLING
+    private final int REQ_PERMISSION = 888;
 
-                    textView.setText(""+distanceInmeters+"meters");
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED);
+    }
 
-//                map.addMarker(new MarkerOptions().position(latLng).title(player.getPlayerName()));
+    // Asks for permission
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                REQ_PERMISSION
+        );
+    }
+
+    // Verify user's response of the permission requested
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    getLastKnownLocation();
+
+                } else {
+                    // Permission denied
+                    Log.w(TAG, "permissionsDenied()");
                 }
+                break;
             }
         }
     }
+    //-------------PERMISSIONS HANDLING FINISHED -------------
 
 
+    private LocationRequest locationRequest;
+    //    // Defined in mili seconds.
+//    // This number in extremely low, and should be used only for debug
+    private final int UPDATE_INTERVAL = 60;
+    private final int FASTEST_INTERVAL = 40;
+
+    // Start location Updates
+    private void startLocationUpdates() {
+        Log.i(TAG, "startLocationUpdates()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+        if (checkPermission())
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged [" + location + "]");
+        lastLocation = location;
+        writeActualLocation(location);
+    }
+
+    // GoogleApiClient.ConnectionCallbacks connected
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "onConnected()");
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            getLastKnownLocation();
+        } else {
+            askPermission();
+        }
+
+    }
 
 
+    // GoogleApiClient.ConnectionCallbacks suspended
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, "onConnectionSuspended()");
+    }
 
+    // GoogleApiClient.OnConnectionFailedListener fail
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.w(TAG, "onConnectionFailed()");
+    }
+
+    // Get last known location
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation()");
+        if (checkPermission()) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (lastLocation != null) {
+                Log.i(TAG, "LasKnown location. " +
+                        "Long: " + lastLocation.getLongitude() +
+                        " | Lat: " + lastLocation.getLatitude());
+                writeLastLocation();
+                startLocationUpdates();
+            } else {
+                Log.w(TAG, "No location retrieved yet");
+                startLocationUpdates();
+            }
+        } else askPermission();
+    }
+
+    private void writeActualLocation(Location location) {
+
+//        String key;
+//        key = root.child("players").getKey();
+//        Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
+        String playerReferenceId = getIntent().getStringExtra("playerId");
+        DatabaseReference updatePlayerLocation = databaseReference.child(playerReferenceId);
+        updatePlayerLocation.child("latitude").setValue(location.getLatitude());
+        updatePlayerLocation.child("longitude").setValue(location.getLongitude());
+        Log.d(TAG, "writeActualLocation: " + location.getLatitude() + location.getLongitude());
+
+        float distanceInmeters = flagLocation.distanceTo(location);
+//        Log.d(TAG, "writeActualLocation: $$$$$$$$$$$$$$$" + distanceInmeters);
+        if(textView.getText().toString().isEmpty()) {
+            textView.setText("");
+        }
+        else {
+            textView.setText("" + distanceInmeters + "meters");
+
+        }
+
+    }
+
+    private void writeLastLocation() {
+        writeActualLocation(lastLocation);
+    }
 
 
 }
--- app/src/main/java/kanedenzil/playerapp/StartActivity.java
+++ app/src/main/java/kanedenzil/playerapp/StartActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -32,16 +33,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class StartActivity extends AppCompatActivity implements LocationListener, GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,View.OnClickListener {
+public class StartActivity extends AppCompatActivity {
 
 
     EditText nameOfPerson;
     Button submit;
     Spinner spinnerTeam;
 
-    FirebaseDatabase db;
-    DatabaseReference root;
     private ChildEventListener childEventListener;
     List<Player> players = new ArrayList<>();
 
@@ -59,7 +57,6 @@ public class StartActivity extends AppCompatActivity implements LocationListener
         return intent;
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,203 +66,21 @@ public class StartActivity extends AppCompatActivity implements LocationListener
         submit = (Button) findViewById(R.id.submit);
         spinnerTeam = (Spinner) findViewById(R.id.teamNames);
 
-        db = FirebaseDatabase.getInstance();
-        root = db.getReference();
 
         submit.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
                 submitClicked();
-
             }
         });
 
-        // create GoogleApiClient
-        createGoogleApi();
 
 
-//        listenForEvent();
     }
-
-    // Create GoogleApiClient instance
-    private void createGoogleApi() {
-        Log.d(TAG, "createGoogleApi()");
-        if (googleApiClient == null) {
-            googleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-    }
-
-    //Connecting GooGle api client on start()
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Call GoogleApiClient connection when starting the Activity
-        googleApiClient.connect();
-    }
-
-    //Disconnecting google api client on start()
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // Disconnect GoogleApiClient when stopping Activity
-        googleApiClient.disconnect();
-    }
-
-    // MARK : PERMISSIONS HANDLING
-    private final int REQ_PERMISSION = 888;
-
-    // Check for permission to access Location
-    private boolean checkPermission() {
-        Log.d(TAG, "checkPermission()");
-        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED);
-    }
-
-    // Asks for permission
-    private void askPermission() {
-        Log.d(TAG, "askPermission()");
-        ActivityCompat.requestPermissions(
-                this,
-                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                REQ_PERMISSION
-        );
-    }
-
-    // Verify user's response of the permission requested
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG, "onRequestPermissionsResult()");
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQ_PERMISSION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission granted
-                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-                    getLastKnownLocation();
-
-                } else {
-                    // Permission denied
-                    Log.w(TAG, "permissionsDenied()");
-                }
-                break;
-            }
-        }
-    }
-    //-------------PERMISSIONS HANDLING FINISHED -------------
-
-
-    private LocationRequest locationRequest;
-    //    // Defined in mili seconds.
-//    // This number in extremely low, and should be used only for debug
-    private final int UPDATE_INTERVAL = 500;
-    private final int FASTEST_INTERVAL = 100;
-
-    // Start location Updates
-    private void startLocationUpdates() {
-        Log.i(TAG, "startLocationUpdates()");
-        locationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(UPDATE_INTERVAL)
-                .setFastestInterval(FASTEST_INTERVAL);
-
-        if (checkPermission())
-            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d(TAG, "onLocationChanged [" + location + "]");
-        lastLocation = location;
-        writeActualLocation(location);
-    }
-
-    // GoogleApiClient.ConnectionCallbacks connected
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.i(TAG, "onConnected()");
-//        getLastKnownLocation();
-    }
-
-
-    // GoogleApiClient.ConnectionCallbacks suspended
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.w(TAG, "onConnectionSuspended()");
-    }
-
-    // GoogleApiClient.OnConnectionFailedListener fail
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.w(TAG, "onConnectionFailed()");
-    }
-
-    // Get last known location
-    private void getLastKnownLocation() {
-        Log.d(TAG, "getLastKnownLocation()");
-        if (checkPermission()) {
-            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-            if (lastLocation != null) {
-                Log.i(TAG, "LasKnown location. " +
-                        "Long: " + lastLocation.getLongitude() +
-                        " | Lat: " + lastLocation.getLatitude());
-                writeLastLocation();
-                startLocationUpdates();
-            } else {
-                Log.w(TAG, "No location retrieved yet");
-                startLocationUpdates();
-            }
-        } else askPermission();
-    }
-
-    private void writeActualLocation(Location location) {
-
-        String key;
-        key = root.child("players").getKey();
-        Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
-//        Log.d(TAG, "####################################### " + key);
-//        root.child("players").child(uniqueReferenceID).child("latitude").setValue(location.getLatitude());
-//        root.child("players").child(uniqueReferenceID).child("longitude").setValue(location.getLongitude());
-
-
-//        DatabaseReference updatePlayerLocation = databaseReference.child(databaseReference.getKey());
-        databaseReference.child("latitude").setValue(location.getLatitude());
-        databaseReference.child("longitude").setValue(location.getLongitude());
-        Log.d(TAG, "writeActualLocation: " + location.getLatitude() + location.getLongitude());
-//        markerLocation(new LatLng(location.getLatitude(), location.getLongitude()));
-    }
-
-    private void writeLastLocation() {
-        writeActualLocation(lastLocation);
-    }
-
-//    private Marker locationMarker;
-//    private void markerLocation(LatLng latLng) {
-//        Log.i(TAG, "markerLocation("+latLng+")");
-//        String title = latLng.latitude + ", " + latLng.longitude;
-//        MarkerOptions markerOptions = new MarkerOptions()
-//                .position(latLng)
-//                .title(title);
-//
-//            if ( locationMarker != null )
-//                locationMarker.remove();
-//            locationMarker = map.addMarker(markerOptions);
-//            float zoom = 14f;
-//            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
-//            map.animateCamera(cameraUpdate);
-//
-//    }
 
 
 //when submit button is clicked
-
-
     public void submitClicked() {
         String name = nameOfPerson.getText().toString().trim();
         String team = spinnerTeam.getSelectedItem().toString();
@@ -276,18 +91,12 @@ public class StartActivity extends AppCompatActivity implements LocationListener
 
             players.add(player);
             databaseReference.setValue(player);
-            if (checkPermission()) {
-                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-                getLastKnownLocation();
-            } else {
-                askPermission();
-            }
 
             Toast.makeText(this, "Player is added", Toast.LENGTH_LONG).show();
 
-
             Intent intent = new Intent(this, DirectionActivity.class);
             intent.putExtra("name", nameOfPerson.getText().toString());
+            intent.putExtra("playerId", databaseReference.getKey());
             intent.putExtra("team", team);
             startActivity(intent);
 
@@ -298,61 +107,5 @@ public class StartActivity extends AppCompatActivity implements LocationListener
 
     }
 
-    @Override
-    public void onClick(View v) {
-
-    }
-
-//    public void listenForEvent(){
-//        if(childEventListener == null) {
-//            childEventListener = new ChildEventListener() {
-//                @Override
-//                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-//                    String key = dataSnapshot.getKey();
-//                    Player p = dataSnapshot.getValue(Player.class);
-//                    Log.d(TAG, "onChildChanged: CHILD HAS CHANGED");
-//                    for(Player pl : players){
-//                        if(pl.getPlayerId().equals(key)){
-//                            pl.latitude = p.latitude;
-//                            pl.longitude = p.longitude;
-//                            break;
-//                        }
-//                    }
-//
-//                }
-//
-//                @Override
-//                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-//                    String key = dataSnapshot.getKey();
-//                    Player p = dataSnapshot.getValue(Player.class);
-//                    Log.d(TAG, "onChildChanged: CHILD HAS CHANGED");
-//                    for(Player pl : players){
-//                        if(pl.getPlayerId().equals(key)){
-//                            pl.latitude = p.latitude;
-//                            pl.longitude = p.longitude;
-//                            break;
-//                        }
-//                    }
-//
-//                }
-//
-//                @Override
-//                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-//
-//                }
-//
-//                @Override
-//                public void onChildRemoved(DataSnapshot dataSnapshot) {
-//
-//                }
-//
-//                @Override
-//                public void onCancelled(DatabaseError databaseError) {
-//
-//                }
-//            };
-//        }
-//        root.child("players").addChildEventListener(childEventListener);
-//    }
 }
 
